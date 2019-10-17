fdae675ae60a238a60407ce9e44f944d04362914
RaVortex00 <49288670+RaVortex00@users.noreply.github.com>
2019-10-17 18:50:20 +0300

    Test 2
    Test to feature branch

--- ROBO Folder/app/src/main/java/com/stimdc/robocode/MainActivity.java
+++ ROBO Folder/app/src/main/java/com/stimdc/robocode/MainActivity.java
@@ -1,7 +1,9 @@
 package com.stimdc.robocode;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "text");
+        Log.d("Yeah");
     }
 }
